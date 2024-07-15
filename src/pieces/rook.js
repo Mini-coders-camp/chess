120a68dcb6eea9a2422616abb377589f07245483
NataliaCiesla <75168436+NataliaCiesla@users.noreply.github.com>
2024-07-15 22:38:00 +0200

    Update rook.js

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -10,6 +10,9 @@ class Rook extends Piece {
     const possibleMoves = [];
   for (let i = 1; i < 8; i++) {
       if (this.row - i >= 0) possibleMoves.push([this.row - i, this.column]);
+      if (this.row + i < 8) possibleMoves.push([this.row + i, this.column]);
+      if (this.column - i >= 0) possibleMoves.push([this.row, this.column - i]);
+      if (this.column + i < 8) possibleMoves.push([this.row, this.column + i]);
     }
     return possibleMoves;
   }
