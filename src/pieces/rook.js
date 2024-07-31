2f2ff0c393727d64ef79a1c4a2177d60540fe8db
Magdalena Kita <magdalenakita96@gmail.com>
2024-07-31 10:29:40 +0200

    Modified code that rook cannot pass through other pieces of the same colour

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -1,18 +1,39 @@
 import Piece from './piece';
-
 class Rook extends Piece {
   constructor(row, column, side) {
     super(row, column, side);
     this.name = 'rook';
     this.display = `<i class="fas fa-chess-rook ${side}"></i>`; //fontawesome rook
   }
-  findLegalMoves() {
-    const possibleMoves = [];
-  for (let i = 1; i < 8; i++) {
-      if (this.row - i >= 0) possibleMoves.push([this.row - i, this.column]);
-      if (this.row + i < 8) possibleMoves.push([this.row + i, this.column]);
-      if (this.column - i >= 0) possibleMoves.push([this.row, this.column - i]);
-      if (this.column + i < 8) possibleMoves.push([this.row, this.column + i]);
+  
+  findLegalMoves(board) {
+    const possibleMoves = [];   
+
+    const directions = [
+      { row: -1, col: 0 }, // Up
+      { row: 1, col: 0 },  // Down
+      { row: 0, col: -1 }, // Left
+      { row: 0, col: 1 }   // Right
+    ];
+
+    for (const { row: dRow, col: dCol } of directions) {
+      for (let i = 1; i < 8; i++) {
+        const newRow = this.row + i * dRow;
+        const newCol = this.column + i * dCol;
+
+        if (newRow < 0 || newRow >= 8 || newCol < 0 || newCol >= 8) break;
+
+        const targetSquare = board.getSquare(newRow, newCol);
+
+        if (!targetSquare.piece) {
+          possibleMoves.push([newRow, newCol]);
+        } else if (targetSquare.piece?.side !== this.side) {
+          possibleMoves.push([newRow, newCol]);
+          break;
+        } else if (targetSquare.piece?.side === this.side) {
+          break;
+        }               
+      }
     }
     return possibleMoves;
   }
