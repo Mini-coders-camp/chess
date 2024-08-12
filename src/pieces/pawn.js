6ccaa35eea008037f94b1b85d512442009080365
NataliaCiesla <75168436+NataliaCiesla@users.noreply.github.com>
2024-08-12 22:46:12 +0200

    Pawn update

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -1,5 +1,5 @@
 import Piece from './piece';
-
+import Board from '../board';
 class Pawn extends Piece {
   constructor(row, column, side) {
     super(row, column, side);
@@ -7,19 +7,71 @@ class Pawn extends Piece {
     this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
   }
 
-  findLegalMoves() {
+  findLegalMoves(board) {
     const possibleMoves = [];
-    
+
     if (this.side == 'white') {
-      if (this.row - 1 >= 0) possibleMoves.push([this.row - 1, this.column]);
+      for (let i = 1; i <= 7; i++)
+        if (this.row - 1 >= 0) {
+          if (board.getSquare(this.row - i, this.column).piece == null) {
+            possibleMoves.push([this.row - 1, this.column]);
+          } else {
+          }
+          break;
+        }
       if (this.row - 2 >= 0 && this.row == 6) possibleMoves.push([this.row - 2, this.column]);
     }
-
+    for (let i = 1; i < 2; i++) {
+      if (this.row - i >= 0 && this.column - i >= 0) {
+        if (board.getSquare(this.row - i, this.column - i).piece) {
+          if (board.getSquare(this.row - i, this.column - i).piece.side !== this.side) {
+            possibleMoves.push([this.row - i, this.column - i]);
+          }
+          break;
+        }
+      }
+    }
+    for (let i = 1; i < 2; i++) {
+      if (this.row - i >= 0 && this.column - i >= 0) {
+        if (board.getSquare(this.row - i, this.column + i).piece) {
+          if (board.getSquare(this.row - i, this.column + i).piece.side !== this.side) {
+            possibleMoves.push([this.row - i, this.column + i]);
+          }
+          break;
+        }
+      }
+    }
     if (this.side == 'black') {
-      if (this.row + 1 <= 7) possibleMoves.push([this.row + 1, this.column]);
+      for (let i = 1; i <= 7; i++)
+        if (this.row + 1 <= 7) {
+          if (board.getSquare(this.row + i, this.column).piece == null) {
+            possibleMoves.push([this.row + 1, this.column]);
+          } else {
+          }
+          break;
+        }
       if (this.row + 2 <= 7 && this.row == 1) possibleMoves.push([this.row + 2, this.column]);
     }
-
+    for (let i = 1; i < 2; i++) {
+      if (this.row + i <= 7 && this.column + i <= 7) {
+        if (board.getSquare(this.row + i, this.column + i).piece) {
+          if (board.getSquare(this.row + i, this.column + i).piece.side !== this.side) {
+            possibleMoves.push([this.row + i, this.column + i]);
+          }
+          break;
+        }
+      }
+    }
+    for (let i = 1; i < 2; i++) {
+      if (this.row + i <= 7 && this.column - i <= 7) {
+        if (board.getSquare(this.row + i, this.column - i).piece) {
+          if (board.getSquare(this.row + i, this.column - i).piece.side !== this.side) {
+            possibleMoves.push([this.row + i, this.column - i]);
+          }
+          break;
+        }
+      }
+    }
     return possibleMoves;
   }
 
