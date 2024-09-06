5c8398ef1aca30173181ec89105e39ba6c40b935
Agata <agata.sianozecka@gmail.com>
2024-09-06 13:42:50 +0200

    optional chaning line 28

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -9,25 +9,25 @@ class King extends Piece {
   findLegalMoves(board) {
     const possibleMoves = [];
     const directions = [
-      [0, -1],  // left
-      [0, 1],   // right
-      [-1, 0],  // down
-      [1, 0],   // up
-      [1, 1],   // up right
+      [0, -1], // left
+      [0, 1], // right
+      [-1, 0], // down
+      [1, 0], // up
+      [1, 1], // up right
       [-1, -1], // down left
-      [1, -1],  // up left
-      [-1, 1],  // down right   
+      [1, -1], // up left
+      [-1, 1], // down right
     ];
 
-    directions.forEach(directions => {
+    directions.forEach((directions) => {
       const newRow = this.row + directions[0];
       const newColumn = this.column + directions[1];
 
-      if (newRow >=0 && newRow <=7 && newColumn >= 0 && newColumn <= 7) {
+      if (newRow >= 0 && newRow <= 7 && newColumn >= 0 && newColumn <= 7) {
         const targetSquare = board.getSquare(newRow, newColumn);
-        if (!targetSquare.piece || targetSquare.piece.side !== this.side) {
+        if (targetSquare.piece?.side !== this.side) {
           possibleMoves.push([newRow, newColumn]);
-        }           
+        }
       }
     });
 
